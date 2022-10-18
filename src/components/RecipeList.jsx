fc997ba66f66fc85099180d58e2b0a7664f659e9
Richard Bausek <richardb@uniform.dev>
2022-10-18 02:12:37 +0200

    fix optional param for recipe list

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -9,7 +9,7 @@ export default function RecipeList ({ component }) {
         <section className='mt-16 bg-slate-100 p-8 rounded-lg mb-16'>
             <h2 className='text-4xl text-center mb-16 font-bold text-gray-900'>{title}</h2>
             <article className="lg:grid lg:gap-4 lg:grid-cols-3 pb-8 lg:pb-0">
-                {component.parameters.recipes.value?.map((recipe, index) => (
+                {component.parameters.recipes?.value?.map((recipe, index) => (
                     <RecipeCard key={index} recipe={recipe} />
 
                 ))}
