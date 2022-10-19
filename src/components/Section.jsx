5674b0df9d74751fe43400e42e17863237c5c4ae
Richard Bausek <richardb@uniform.dev>
2022-10-19 16:25:22 +0200

    improve section component and add to storybook

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,12 +1,13 @@
 import { Slot } from "@uniformdev/canvas-react";
+import slugify from "slugify";
 
-export default function Section({ title, children }) {
-
+export default function Section({ title = '', children }) {
+  const id = slugify(title);
   return (
-    <section className="section bg-gray">
+    <section className="section bg-gray" id={id}>
 
-      <div className="container mx-auto max-w-lg py-8 lg:py-20">
-        <h2 className="text-4xl text-center mb-16 font-bold text-gray-900">
+      <div className="container mx-auto max-w-xxl py-8 lg:py-20">
+        <h2 className="text-4xl text-left mb-16 font-bold text-gray-900">
           {title}
         </h2>
         
