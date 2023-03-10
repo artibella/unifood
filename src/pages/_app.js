2152d1ae0ee5c34536371d54110d12a8287a009e
Richard Bausek <richardb@uniform.dev>
2023-03-10 15:00:34 +0100

    next 13 upgrade part 2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,71 +1,36 @@
-import { UniformContext } from "@uniformdev/context-react";
-import { Context, enableContextDevTools } from "@uniformdev/context";
-import manifest from "../lib/contextManifest.json";
-import getConfig from "next/config";
-import Layout from "../components/layout";
-import Script from "next/script";
-import * as gtag from "../lib/gtag";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import "../styles/globals.css";
-import "../styles/page.css";
-import { Html } from "next/document";
-import Head from "next/head";
+import { UniformContext } from '@uniformdev/context-react';
+import Layout from '../components/layout';
+import { createUniformContext } from '../lib/context/uniformContext';
 
-const {
-  publicRuntimeConfig: { gtmStreamId },
-} = getConfig();
+import '../styles/globals.css';
+import '../styles/page.css';
+// import fonts
+import { Source_Serif_Pro, Inter } from 'next/font/google';
 
-const context = new Context({
-  manifest,
-  defaultConsent: true,
-  plugins: [
-    enableContextDevTools(),
-  ]
+const sourceSerif = Source_Serif_Pro({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  variable: '--font-source-serif-pro',
 });
 
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
+const clientContext = createUniformContext();
 
-function MyApp({ Component, pageProps, scoring }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
-  
+function UnifoodApp({ Component, pageProps, serverUniformContext }) {
   return (
-    <>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        />
-        <Script
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-
-      <Layout>
-        <UniformContext context={context}>
+    <UniformContext context={serverUniformContext ?? clientContext}>
+      <main className={`${sourceSerif.variable} ${inter.variable} font-sans`}>
+        <Layout>
           <Component {...pageProps} />
-        </UniformContext>
-      </Layout>
-    </>
+        </Layout>
+      </main>
+    </UniformContext>
   );
 }
 
-export default MyApp;
+export default UnifoodApp;
