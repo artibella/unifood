cc3d3291b8d277ab83ea83e085e06664e3a04b93
Richard Bausek <richardb@uniform.dev>
2023-03-20 13:26:16 +0100

    debugging recipe list

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,22 +1,15 @@
-import Link from 'next/link'
+import Link from 'next/link';
 import { Card } from 'react-daisyui';
 
 export default function RecipeCard({ recipe }) {
   const recipeImage = `${recipe.fields?.image?.fields.file.url}?w=600&h=400&fit=thumb`;
-  const recipeImageAlt = `${recipe.fields?.title}`
+  const recipeImageAlt = `${recipe.fields?.title}`;
 
   return (
-    <Card className='bg-white'>
-      <Card.Image
-        src={recipeImage}
-        alt={recipeImageAlt}
-      />
+    <Card className="bg-white">
+      <Card.Image src={recipeImage} alt={recipeImageAlt} />
       <Card.Body>
-        <Card.Title tag="h2">
-          <Link href={`/recipes/${recipe.fields.slug}`} className="hover:underline">
-            {recipe.fields.title}
-          </Link>
-          </Card.Title>
+        <Card.Title tag="h2">{recipe.fields.title}</Card.Title>
       </Card.Body>
     </Card>
   );
