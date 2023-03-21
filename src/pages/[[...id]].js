63d88f93d9fb4e5034c58119e70a3f587a94de06
Richard Bausek <richardb@uniform.dev>
2023-03-21 14:10:52 +0100

    change rendering to use Uniform Next helpers

--- src/pages/[[...id]].js
+++ src/pages/[[...id]].js
@@ -1,88 +1,51 @@
 import React from 'react';
 import getConfig from 'next/config';
-import { enhance, CANVAS_DRAFT_STATE, CANVAS_PUBLISHED_STATE } from '@uniformdev/canvas';
-import { useContextualEditing, createApiEnhancer } from '@uniformdev/canvas-react';
+import { enhance } from '@uniformdev/canvas';
 import { canvasClient } from '../lib/canvas';
 import { getEnhancers } from '../lib/enhancers/enhancers';
 import { projectMapClient } from '../lib/projectMap';
 import { compositionRenderer } from '../compositions/compositionRenderer';
+import { withUniformGetStaticProps, withUniformGetStaticPaths } from '@uniformdev/canvas-next/project-map';
 
 const {
-  serverRuntimeConfig: { projectId, projectMapId },
+  serverRuntimeConfig: { projectMapId },
 } = getConfig();
 
-export default function DynamicComposition({ composition: initialCompositionValue }) {
-  // enable contextual editing
-  const { composition } = useContextualEditing({
-    initialCompositionValue,
-    enhance: createApiEnhancer({
-      apiUrl: '/api/preview',
-    }),
-  });
+export default function DynamicComposition({ composition }) {
   // get composition type
   const CompositionType = compositionRenderer(composition);
+
   return <CompositionType composition={composition} />;
 }
 
-export const getStaticProps = async context => {
-  const slug = context?.params?.id || '';
-  const slugString = Array.isArray(slug) ? slug.join('/') : slug;
-  const { preview } = context;
-
-  const { composition } = await canvasClient.unstable_getCompositionByNodePath({
-    projectMapId,
-    projectMapNodePath: slugString ? `/${slugString}` : '/',
-    state: process.env.NODE_ENV === 'development' || preview ? CANVAS_DRAFT_STATE : CANVAS_PUBLISHED_STATE,
+export const getStaticProps = withUniformGetStaticProps({
+  param: 'id', // to match with file name
+  client: canvasClient,
+  projectMapId: projectMapId,
+  requestOptions: {
     unstable_resolveData: true,
-  });
+  },
+  callback: async (context, composition) => {
+    if (composition) {
+      // run enhancers
+      await enhance({ composition, enhancers: getEnhancers(), context });
+    } else {
+      return {
+        notFound: true,
+      };
+    }
+    return {
+      props: {
+        composition,
+      },
+    };
+  },
+});
 
-  // return 404 if no composition is found
-  if (!composition) {
-    return { notFound: true };
-  }
-
-  await enhance({
-    composition,
-    enhancers: getEnhancers(),
-    context: { preview },
-  });
-
-  return {
-    props: {
-      composition,
-    },
-  };
-};
-
-export async function getStaticPaths() {
-  const reservedSlugs = ['/howtos'];
-
-  const { nodes } = await projectMapClient.getNodes({
-    projectId,
-    projectMapId,
-  });
-
-  // TODO: this needs improvement to scale beyond 100 compositions
-  const { compositions } = await canvasClient.getCompositionList({
-    projectId,
-    state: CANVAS_PUBLISHED_STATE,
-  });
-  const compositonIds = compositions.map(item => item.composition._id);
-
-  const paths = nodes.filter(node => {
-    const path = node.path;
-    const hasComposition =
-      node.type === 'composition' && node.compositionId && compositonIds.includes(node.compositionId);
-    const isReservedSlug = reservedSlugs.filter(slug => path.includes(slug)).length > 0;
-    return hasComposition && !isReservedSlug;
-  });
-
-  const staticPaths = paths.map(node => {
-    return `${node.path}`;
-  });
-
-  return {
-    paths: staticPaths || [],
-    fallback: true,
-  };
-}
+export const getStaticPaths = withUniformGetStaticPaths({
+  client: projectMapClient,
+  callback: async nodes => {
+    const skipPathNames = ['/howtos/'];
+    return nodes.filter(node => !skipPathNames.find(path => node.path.startsWith(path)));
+  },
+});
